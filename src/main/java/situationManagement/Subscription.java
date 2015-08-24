9ee374f43eabb30acd2121d53a6c1391cc18aae6
Stefan <fuerstsn@studi.informatik.uni-stuttgart.de>
2015-08-24 16:23:59 +0200

    Beim Shutdown gibt es jetzt keine Exception mehr. Resolves #36

--- src/main/java/situationManagement/Subscription.java
+++ src/main/java/situationManagement/Subscription.java
@@ -24,129 +24,132 @@ import situationHandling.storage.datatypes.Situation;
  */
 class Subscription {
 
-	/**
-	 * The logger for this class.
-	 */
-	private static final Logger logger = Logger.getLogger(Subscription.class);
+    /**
+     * The logger for this class.
+     */
+    private static final Logger logger = Logger.getLogger(Subscription.class);
 
-	/** The subscription count. The number of times a subscription was made. */
-	private int subscriptionCount = 0;
+    /** The subscription count. The number of times a subscription was made. */
+    private int subscriptionCount = 0;
 
-	/** The srs communicator to create/delete subscriptions. */
-	private final SRSCommunicator srsCommunicator;
+    /** The srs communicator to create/delete subscriptions. */
+    private final SRSCommunicator srsCommunicator;
 
-	/**
-	 * The address of the situation handler. The situation changes will be sent
-	 * to this URL.
-	 */
-	private final URL ownAddress;
+    /**
+     * The address of the situation handler. The situation changes will be sent
+     * to this URL.
+     */
+    private final URL ownAddress;
 
-	/**
-	 * The situation of this subscription.
-	 */
-	private final Situation subscribeSituation;
+    /**
+     * The situation of this subscription.
+     */
+    private final Situation subscribeSituation;
 
-	/**
-	 * The Executor service to run the subscriber thread.
-	 */
-	private ExecutorService subscriberPool = Executors.newSingleThreadExecutor();
+    /**
+     * The Executor service to run the subscriber thread.
+     */
+    private ExecutorService subscriberPool = Executors.newSingleThreadExecutor();
 
-	/**
-	 * The result of the subscriber thread.
-	 */
-	private Future<Boolean> subscriberResult;
+    /**
+     * The result of the subscriber thread.
+     */
+    private Future<Boolean> subscriberResult;
 
-	/**
-	 * Creates a new subscription. Creates a subscription at the srs.
-	 */
-	Subscription(SRSCommunicator srsCommunicator, URL ownAddress, Situation subscribeSituation) {
-		subscriptionCount = 1;
-		this.srsCommunicator = srsCommunicator;
-		this.ownAddress = ownAddress;
-		this.subscribeSituation = subscribeSituation;
-		Subscriber subscriber = new Subscriber(this);
-		subscriberResult = subscriberPool.submit(subscriber);
-	}
+    /**
+     * Creates a new subscription. Creates a subscription at the srs.
+     */
+    Subscription(SRSCommunicator srsCommunicator, URL ownAddress, Situation subscribeSituation) {
+	subscriptionCount = 1;
+	this.srsCommunicator = srsCommunicator;
+	this.ownAddress = ownAddress;
+	this.subscribeSituation = subscribeSituation;
+	Subscriber subscriber = new Subscriber(this);
+	subscriberResult = subscriberPool.submit(subscriber);
+    }
 
-	/**
-	 * Gives the information if this subscription is still required.
-	 *
-	 * @return true, if it is required. When false, the subscription can be
-	 *         deleted.
-	 */
-	boolean subsriptionsAvailable() {
-		return subscriptionCount > 0;
-	}
+    /**
+     * Gives the information if this subscription is still required.
+     *
+     * @return true, if it is required. When false, the subscription can be
+     *         deleted.
+     */
+    boolean subsriptionsAvailable() {
+	return subscriptionCount > 0;
+    }
 
-	/**
-	 * Adds a subscription.
-	 */
-	void addSubscription() {
-		subscriptionCount++;
-	}
+    /**
+     * Adds a subscription.
+     */
+    void addSubscription() {
+	subscriptionCount++;
+    }
 
-	/**
-	 * Removes a subsription. Removes the subscription from the SRS, if this was
-	 * the last subscription or permanentlyy is set to true.
-	 * 
-	 * @param permanently
-	 *            true, if the subscription is to be permanently removed, i.e.
-	 *            the subscription is also deleted from the srs.
-	 */
-	void removeSubsription(boolean permanently) {
-		subscriptionCount--;
-		if (!subsriptionsAvailable() || permanently) {
-			try {
-				if (!subscriberResult.isDone()) {
-					subscriberResult.cancel(true);
-				}
-
-				if (!subscriberResult.isCancelled() && subscriberResult.get()) {
-					srsCommunicator.unsubscribe(subscribeSituation, ownAddress);
-				}
-				onSubscriptionCreated();
-			} catch (InterruptedException | ExecutionException e) {
-				logger.error("Error when deleting subscription.", e);
-			}
+    /**
+     * Removes a subsription. Removes the subscription from the SRS, if this was
+     * the last subscription or permanentlyy is set to true.
+     * 
+     * @param permanently
+     *            true, if the subscription is to be permanently removed, i.e.
+     *            the subscription is also deleted from the srs.
+     */
+    void removeSubsription(boolean permanently) {
+	subscriptionCount--;
+	if (!subsriptionsAvailable() || permanently) {
+	    try {
+		// chancel subscriber if not done
+		if (!subscriberResult.isDone()) {
+		    subscriberResult.cancel(true);
 		}
-	}
 
-	/**
-	 * Callback when the subscription was successfully created.
-	 */
-	void onSubscriptionCreated() {
-		subscriberPool.shutdown();
+		// if subscriber done, get result and unsubscribe in case of
+		// success
+		if (!subscriberResult.isCancelled() && subscriberResult.get()) {
+		    srsCommunicator.unsubscribe(subscribeSituation, ownAddress);
+		}
+		onSubscriptionCreated();
+	    } catch (InterruptedException | ExecutionException e) {
+		logger.error("Error when deleting subscription.", e);
+	    }
 	}
+    }
 
-	/**
-	 * @return the srsCommunicator used to contact the srs.
-	 */
-	SRSCommunicator getSrsCommunicator() {
-		return srsCommunicator;
-	}
+    /**
+     * Callback when the subscription was successfully created.
+     */
+    void onSubscriptionCreated() {
+	subscriberPool.shutdown();
+    }
 
-	/**
-	 * @return the address of the situation handler
-	 */
-	URL getOwnAddress() {
-		return ownAddress;
-	}
+    /**
+     * @return the srsCommunicator used to contact the srs.
+     */
+    SRSCommunicator getSrsCommunicator() {
+	return srsCommunicator;
+    }
 
-	/**
-	 * @return the situation of this subscription
-	 */
-	Situation getSubscribeSituation() {
-		return subscribeSituation;
-	}
+    /**
+     * @return the address of the situation handler
+     */
+    URL getOwnAddress() {
+	return ownAddress;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "Subscription [subscriptionCount=" + subscriptionCount + "]";
-	}
+    /**
+     * @return the situation of this subscription
+     */
+    Situation getSubscribeSituation() {
+	return subscribeSituation;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+	return "Subscription [subscriptionCount=" + subscriptionCount + "]";
+    }
 
 }
--- src/main/java/situationManagement/SubscriptionHandler.java
+++ src/main/java/situationManagement/SubscriptionHandler.java
@@ -22,138 +22,122 @@ import situationHandling.storage.datatypes.Situation;
  */
 class SubscriptionHandler {
 
-	/** The logger. */
-	private final static Logger logger = Logger.getLogger(SubscriptionHandler.class);
+    /** The logger. */
+    private final static Logger logger = Logger.getLogger(SubscriptionHandler.class);
 
-	/** The subscriptions managed by this handler. */
-	private Map<Situation, Subscription> subscriptions = new HashMap<Situation, Subscription>();
+    /** The subscriptions managed by this handler. */
+    private Map<Situation, Subscription> subscriptions = new HashMap<Situation, Subscription>();
 
-	/** The srs communicator to create/delete subscriptions. */
-	private SRSCommunicator srsCommunicator;
+    /** The srs communicator to create/delete subscriptions. */
+    private SRSCommunicator srsCommunicator;
 
-	/**
-	 * The address of the situation handler. The situation changes will be sent
-	 * to this URL.
-	 */
-	private URL ownAddress;
+    /**
+     * The address of the situation handler. The situation changes will be sent
+     * to this URL.
+     */
+    private URL ownAddress;
 
-	/**
-	 * Instantiates a new subscription handler.
-	 *
-	 * @param ownAddress
-	 *            The address of the situation handler. The situation changes
-	 *            will be sent to this URL.
-	 * @param srsCommunicator
-	 *            the srs communicator to create/delete subscriptions.
-	 */
-	SubscriptionHandler(URL ownAddress, SRSCommunicator srsCommunicator) {
-		this.ownAddress = ownAddress;
-		this.srsCommunicator = srsCommunicator;
+    /**
+     * Instantiates a new subscription handler.
+     *
+     * @param ownAddress
+     *            The address of the situation handler. The situation changes
+     *            will be sent to this URL.
+     * @param srsCommunicator
+     *            the srs communicator to create/delete subscriptions.
+     */
+    SubscriptionHandler(URL ownAddress, SRSCommunicator srsCommunicator) {
+	this.ownAddress = ownAddress;
+	this.srsCommunicator = srsCommunicator;
+    }
+
+    /**
+     * Subscribe to a situation. Only sends a subscription to the SRS for new
+     * subscriptions.
+     *
+     * @param situation
+     *            the situation to subscribe on.
+     */
+    void subscribe(Situation situation) {
+	if (subscriptions.containsKey(situation)) {
+	    subscriptions.get(situation).addSubscription();
+	    logger.trace("Added additional subscription on " + situation);
+	    logger.trace(subscriptions.get(situation).toString());
+	} else {
+	    logger.debug("Creating subscription on " + situation.toString());
+	    subscriptions.put(situation, new Subscription(srsCommunicator, ownAddress, situation));
 	}
 
-	/**
-	 * Subscribe to a situation. Only sends a subscription to the SRS for new
-	 * subscriptions.
-	 *
-	 * @param situation
-	 *            the situation to subscribe on.
-	 */
-	void subscribe(Situation situation) {
-		if (subscriptions.containsKey(situation)) {
-			subscriptions.get(situation).addSubscription();
-			logger.trace("Added additional subscription on " + situation);
-			logger.trace(subscriptions.get(situation).toString());
-		} else {
-			logger.debug("Creating subscription on " + situation.toString());
-			subscriptions.put(situation, new Subscription(srsCommunicator, ownAddress, situation));
-		}
+    }
 
+    /**
+     * Removes the subscription. Only deletes the subscription, if the last
+     * subscription required by the situation handler is deleted.
+     *
+     * @param situation
+     *            the situation for unsubscription
+     */
+    void removeSubscription(Situation situation) {
+	if (subscriptions.containsKey(situation)) {
+	    Subscription subscription = subscriptions.get(situation);
+	    subscription.removeSubsription(false);
+	}
+    }
+
+    /**
+     * Creates a subscription for each rule/handledSituation in the directory.
+     * Can be used to initially create all subscriptions.
+     */
+    void reloadSubscriptions() {
+	subscriptions.clear();
+	// do subscriptions for all rules
+	RuleStorageAccess rsa = StorageAccessFactory.getRuleStorageAccess();
+	for (Rule rule : rsa.getAllRules()) {
+	    subscribe(rule.getSituation());
 	}
 
-	/**
-	 * Removes the subscription. Only deletes the subscription, if the last
-	 * subscription required by the situation handler is deleted.
-	 *
-	 * @param situation
-	 *            the situation for unsubscription
-	 */
-	void removeSubscription(Situation situation) {
-		if (subscriptions.containsKey(situation)) {
-			Subscription subscription = subscriptions.get(situation);
-			subscription.removeSubsription(false);
-			if (!subscription.subsriptionsAvailable()) {
-				deleteSubscription(situation);
-			} else {
-				logger.trace("Removed one subscription on " + situation);
-				logger.trace(subscriptions.get(situation).toString());
-			}
-		}
+	// do subscriptions for all situations handled by endpoints
+	EndpointStorageAccess esa = StorageAccessFactory.getEndpointStorageAccess();
+	for (Endpoint endpoint : esa.getAllEndpoints()) {
+	    for (HandledSituation handledSituation : endpoint.getSituations()) {
+		subscribe(new Situation(handledSituation.getSituationName(),
+			handledSituation.getObjectName()));
+	    }
 	}
 
-	/**
-	 * Deletes a subscription.
-	 *
-	 * @param situation
-	 *            the situation to unsubscribe.
-	 */
-	private void deleteSubscription(Situation situation) {
-		logger.debug("Deleting subscription on " + situation.toString());
-		subscriptions.remove(situation);
+	logger.debug("Subscriptions reloaded:\n" + getSubscriptionsAsString());
+    }
+
+    /**
+     * Deletes all subscriptions.
+     */
+    void deleteAllSubscriptions() {
+	logger.debug("Deleting all subscriptions.");
+	for (Situation situation : subscriptions.keySet()) {
+	    subscriptions.get(situation).removeSubsription(true);
 	}
+	subscriptions.clear();
+    }
 
-	/**
-	 * Creates a subscription for each rule/handledSituation in the directory.
-	 * Can be used to initially create all subscriptions.
-	 */
-	void reloadSubscriptions() {
-		subscriptions.clear();
-		// do subscriptions for all rules
-		RuleStorageAccess rsa = StorageAccessFactory.getRuleStorageAccess();
-		for (Rule rule : rsa.getAllRules()) {
-			subscribe(rule.getSituation());
-		}
-
-		// do subscriptions for all situations handled by endpoints
-		EndpointStorageAccess esa = StorageAccessFactory.getEndpointStorageAccess();
-		for (Endpoint endpoint : esa.getAllEndpoints()) {
-			for (HandledSituation handledSituation : endpoint.getSituations()) {
-				subscribe(new Situation(handledSituation.getSituationName(), handledSituation.getObjectName()));
-			}
-		}
-
-		logger.debug("Subscriptions reloaded:\n" + getSubscriptionsAsString());
+    /**
+     * Gets the subscriptions as string. Gives an overview of all current
+     * subscriptions as nicely (?) readable string.
+     *
+     * @return the subscriptions as string
+     */
+    String getSubscriptionsAsString() {
+	StringBuilder sb = new StringBuilder();
+	sb.append("-----------Subscriptions:-----------");
+	sb.append(System.getProperty("line.separator"));
+	sb.append("Total Number of Subscriptions: " + subscriptions.size());
+	sb.append(System.getProperty("line.separator"));
+	for (Situation situation : subscriptions.keySet()) {
+	    sb.append(situation.toString() + " --> " + subscriptions.get(situation).toString());
+	    sb.append(System.getProperty("line.separator"));
 	}
-
-	/**
-	 * Deletes all subscriptions.
-	 */
-	void deleteAllSubscriptions() {
-		logger.debug("Deleting all subscriptions.");
-		for (Situation situation : subscriptions.keySet()) {
-			subscriptions.get(situation).removeSubsription(true);
-			deleteSubscription(situation);
-		}
-	}
-
-	/**
-	 * Gets the subscriptions as string. Gives an overview of all current
-	 * subscriptions as nicely (?) readable string.
-	 *
-	 * @return the subscriptions as string
-	 */
-	String getSubscriptionsAsString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("-----------Subscriptions:-----------");
-		sb.append(System.getProperty("line.separator"));
-		sb.append("Total Number of Subscriptions: " + subscriptions.size());
-		sb.append(System.getProperty("line.separator"));
-		for (Situation situation : subscriptions.keySet()) {
-			sb.append(situation.toString() + " --> " + subscriptions.get(situation).toString());
-			sb.append(System.getProperty("line.separator"));
-		}
-		sb.append("-----------End Subscriptions-----------");
-		sb.append(System.getProperty("line.separator"));
-		return sb.toString();
-	}
+	sb.append("-----------End Subscriptions-----------");
+	sb.append(System.getProperty("line.separator"));
+	return sb.toString();
+    }
 
 }
