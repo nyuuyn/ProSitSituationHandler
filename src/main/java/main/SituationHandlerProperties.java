a28cc780b0e69fe6f2cf3ffc56a98c7f6fa947d8
Stefan <fuerstsn@studi.informatik.uni-stuttgart.de>
2015-10-06 11:19:56 +0200

    Properties Zeug kommentiert. Resolves #43

--- src/main/java/main/SituationHandlerProperties.java
+++ src/main/java/main/SituationHandlerProperties.java
@@ -6,15 +6,36 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ * This class is used to load the situation handler properties from the file
+ * system. It allows other classes to access those properties using getters.
+ * <p>
+ * When a property could not be loaded or is not set in the properites file, a
+ * default value is returned.
+ * 
+ * @author Stefan
+ *
+ */
 public class SituationHandlerProperties {
 
     /** The logger. */
     private static final Logger logger = Logger.getLogger(SituationHandlerProperties.class);
 
-    private static Properties properties = new Properties();
+    /**
+     * The name of the properties file.
+     */
     private static final String PROPERTIES_FILENAME = "situationHandler.config.properties";
 
+    /**
+     * The properties as loaded from the file system.
+     */
+    private static Properties properties = new Properties();
+
     static {
+	/*
+	 * Load the properties.
+	 */
 	InputStream inputStream = null;
 	try {
 	    inputStream = SituationHandlerProperties.class.getResourceAsStream(PROPERTIES_FILENAME);
@@ -31,6 +52,15 @@ public class SituationHandlerProperties {
 	}
     }
 
+    /**
+     * Gets the network port to use.
+     * <p>
+     * Only relevant when the situation handler is used without an application
+     * server or something (e.g. when using the jetty component).
+     * 
+     * @return the network port as specified in the properties or 8081 as the
+     *         default value.
+     */
     public static int getNetworkPort() {
 	try {
 	    return Integer.parseInt(properties.getProperty("situationHandler.network.port"));
@@ -40,9 +70,12 @@ public class SituationHandlerProperties {
     }
 
     /**
-     * Get the maximum file size (in bytes) that is allowed for file upload at
+     * Gets the maximum file size (in bytes) that is allowed for file upload at
      * the rest api. This especially limits the size of plugins that are added
      * to the situation handler using the rest api.
+     * <p>
+     * Only relevant when the situation handler is used without an application
+     * server or something (e.g. when using the jetty component).
      * 
      * @return the value for the file size as specified in the properties or
      *         15_000_000 as the default value.
@@ -56,29 +89,58 @@ public class SituationHandlerProperties {
 	}
     }
 
+    /**
+     * Gets the relative path of the http endpoint that is used to reveive
+     * situation changes.
+     * 
+     * @return the path to the http endpoint as specified in the properties or
+     *         SituationEndpoint as the default value
+     */
     public static String getSituationEndpointPath() {
 	return properties.getProperty("situationHandler.endpoints.SituationEndpointPath",
 		"SituationEndpoint");
     }
 
+    /**
+     * Gets the relative path of the http endpoint that is used to reveive
+     * answers of workflows.
+     * 
+     * @return the path to the http endpoint as specified in the properties or
+     *         AnswerEndpoint as the default value
+     */
     public static String getAnswerEndpointPath() {
 	return properties.getProperty("situationHandler.endpoints.AnswerEndpointPath",
 		"AnswerEndpoint");
     }
 
+    /**
+     * Gets the relative path of the http endpoint that is used to reveive
+     * requests from workflows.
+     * 
+     * @return the path to the http endpoint as specified in the properties or
+     *         RequestEndpoint as the default value
+     */
     public static String getRequestEndpointPath() {
 	return properties.getProperty("situationHandler.endpoints.RequestEndpointPath",
 		"RequestEndpoint");
     }
 
+    /**
+     * Gets the (relative) base path for the rest api.
+     * 
+     * @return the base path as specified in the properties or config as the
+     *         default value.
+     */
     public static String getRestApiBasePath() {
 	return properties.getProperty("situationHandler.rest.basepath", "config");
     }
 
     /**
-     * Get the path to the root folder that contains the web app.
+     * Gets the path to the root folder that contains the web app.
      * <p>
-     * Only relevant when using the jetty component.
+     * Only relevant when the situation handler is used without an application
+     * server or something (e.g. when using the jetty component). When using an
+     * application server, the web app should better be provided by this server.
      * 
      * @return the path of the root folder as specified in the properties or
      *         C:\\app as the default value.
@@ -88,7 +150,7 @@ public class SituationHandlerProperties {
     }
 
     /**
-     * Get the address of the situation recognition system / SitOpt.
+     * Gets the address of the situation recognition system / SitOpt.
      * 
      * @return the value for srs address as specified in the properties or
      *         http://192.168.209.246:10010 as the default value.
@@ -99,8 +161,8 @@ public class SituationHandlerProperties {
     }
 
     /**
-     * Get the path to the plugin folder. Jar-Files in this folder are loaded at
-     * startup.
+     * Gets the path to the plugin folder. Jar-Files in this folder are loaded
+     * at startup.
      * 
      * @return the path to the plugin folder as specified in the properties or
      *         plugins as the default value.
@@ -110,7 +172,7 @@ public class SituationHandlerProperties {
     }
 
     /**
-     * Get the path to the runtime folder. This folder is used to store jars
+     * Gets the path to the runtime folder. This folder is used to store jars
      * that are added at runtime.
      * 
      * @return the path to the runtime folder as specified in the properties or
@@ -120,6 +182,13 @@ public class SituationHandlerProperties {
 	return properties.getProperty("situationHandler.plugins.runtimeFolder", "runtime");
     }
 
+    /**
+     * Gets the default number of max retries for workflow operations. Used when
+     * the workflow request does not specify a number.
+     * 
+     * @return the number as specified in the properties or 2 as the default
+     *         value.
+     */
     public static int getDefaultMaxRetries() {
 	try {
 	    return Integer.parseInt(
@@ -129,6 +198,13 @@ public class SituationHandlerProperties {
 	}
     }
 
+    /***
+     * 
+     * Gets the default size to use for thread pools. Depends on the number of
+     * available CPUs.
+     * 
+     * @return the default size of the thread pool.
+     */
     public static int getDefaultThreadPoolSize() {
 	return Runtime.getRuntime().availableProcessors();
     }
